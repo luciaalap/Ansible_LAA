6ddda69fcad85dc8f57fd771c9bf9262ba9c6856
luciaalap <luciaalap@gmail.com>
2022-04-27 17:50:48 +0000

    add files

---
+++ playbooks/roles/install_hadoop/templates/hadoop-env.sh
@@ -0,0 +1,6 @@
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64
+export HDFS_NAMENODE_USER=hdfs
+export HDFS_DATANODE_USER=hdfs
+export HDFS_SECONDARYNAMENODE_USER=hdfs
+export YARN_RESOURCEMANAGER_USER=hdfs
+export YARN_NODEMANAGER_USER=hdfs
